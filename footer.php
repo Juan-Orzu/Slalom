14bec6a130443ab67e2fd7010ca928519417b1ec
Juan Carlos Angeles Orzuna <juankyorzuna08@gmail.com>
2022-07-01 12:22:28 -0500

    Initial commit

---
+++ footer.php
@@ -0,0 +1,101 @@
+<footer>
+    <div class="col-6 col-sm-3">
+        <a href="/insights.php" title="Insights" class="l-f__menu-item l-f__menu-item--right">
+            Insights
+        </a>
+        <a href="/careers.php" title="Careers" class="l-f__menu-item l-f__menu-item--right">
+            Careers
+        </a>
+        <a href="/locations.php" title="Locations" class="l-f__menu-item l-f__menu-item--right">
+            Locations
+        </a>
+    </div>
+    </div>
+    </div>
+    <div class="col-12 offset-lg-4 col-lg-6 order-lg-3">
+        <span class="l-f__line"></span>
+    </div>
+    <div class="col-12 col-lg-4 order-lg-4">
+        <div class="l-f__menu l-f__menu--social">
+            <a href="https://www.facebook.com/slalomconsulting" title="Facebook" target="_blank"
+                class="l-f__menu-item l-f__menu-item--social">
+                <span class="l-f__menu-item-title">
+                    <svg role="presentation" aria-hidden="true" class=" a-icon icon-facebook ">
+                        <use xlink:href="#icon-facebook"></use>
+                    </svg>
+                </span>
+            </a>
+            <a href="https://twitter.com/slalom" title="Twitter" target="_blank"
+                class="l-f__menu-item l-f__menu-item--social">
+                <span class="l-f__menu-item-title">
+                    <svg role="presentation" aria-hidden="true" class=" a-icon icon-twitter ">
+                        <use xlink:href="#icon-twitter"></use>
+                    </svg>
+                </span>
+            </a>
+            <a href="https://www.instagram.com/slalomconsulting/" title="Instagram" target="_blank"
+                class="l-f__menu-item l-f__menu-item--social">
+                <span class="l-f__menu-item-title">
+                    <svg role="presentation" aria-hidden="true" class=" a-icon icon-instagram ">
+                        <use xlink:href="#icon-instagram"></use>
+                    </svg>
+                </span>
+            </a>
+            <a href="https://www.linkedin.com/company/slalom-consulting" title="LinkedIn" target="_blank"
+                class="l-f__menu-item l-f__menu-item--social">
+                <span class="l-f__menu-item-title">
+                    <svg role="presentation" aria-hidden="true" class=" a-icon icon-linkedin ">
+                        <use xlink:href="#icon-linkedin"></use>
+                    </svg>
+                </span>
+            </a>
+        </div>
+    </div>
+    <div class="col-12 col-lg-3 order-lg-1">
+        <div class="l-f__content l-f__content--left">
+            <p></p>
+            <p>Have you seen what weve been working on at <a href="https://www.slalombuild.com/" rel="noopener"
+                    target="_blank">Slalom Build</a>?</p>
+            <p>&nbsp;</p>
+            <p>Visit our Medium publication <a href="https://medium.com/the-slowdown" rel="noreferrer noopener"
+                    target="_blank" title="https://medium.com/the-slowdown">The Slowdown</a> for explorations of
+                diversity, inclusion, and the future.</p>
+            <p></p>
+        </div>
+    </div>
+    <div class="col-12 col-lg-2 order-lg-5">
+        <div class="l-f__menu">
+            <a href="/privacy-policy.php" title="Privacy policy" class="l-f__menu-item ">
+                Privacy policy
+            </a>
+            <a href="/terms-of-use.php" title="Terms of use" class="l-f__menu-item ">
+                Terms of use
+            </a>
+            <a href="#" title="Cookie settings" class="l-f__menu-item  optanon-show-settings">
+                Cookie settings
+            </a>
+        </div>
+    </div>
+    <div class="col-12 col-lg-2 order-lg-7">
+        <p class="l-f__quote h-text-medium">
+            Love your future
+            <span class="l-footer_shape">
+                <svg class=" a-icon icon-shape ">
+                    <use xlink:href="#icon-shape"></use>
+                </svg>
+            </span>
+        </p>
+    </div>
+    <div class="col-12 col-lg-4 order-lg-6">
+        <p class="l-f__content l-f__content--disclaimer">
+        </p>
+        <p><span style="font-family:arial;">©</span> 2022 Slalom, LLC</p>
+        <p><br>
+            <br>
+            All rights reserved
+        </p>
+        <p></p>
+    </div>
+    </div>
+    </div>
+</footer>
---
+++ menu.php
@@ -0,0 +1,92 @@
+<html lang="en">
+
+<link rel="stylesheet" href="sources/style.css">
+
+<header>
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <a href="https://www.slalom.com/" title="Slalom" class="l-h__logo h-link">
+                    <svg role="presentation" aria-hidden="true" class=" a-icon icon-slalom ">
+                        <use xlink:href="#icon-slalom"></use>
+                    </svg>
+                </a>
+                <nav class="l-h__m">
+                    <div class="l-h__m-container">
+                        <div class="l-h__m-content">
+                            <ul class="l-h__m-list">
+                                <li class="l-h__m-li l-h__m-li-row-2 l-h__m-li--home">
+                                    <a href="/" target="" title="Home" class="l-h__m-link a-link--wline-span">
+                                        <span class="l-h__m-link-w">Home</span>
+                                    </a>
+                                </li>
+                                <li class="l-h__m-li l-h__m-li-row-2 ">
+                                    <a href="/what-we-do.php" target="" title="What we do"
+                                        class="l-h__m-link a-link--wline-span">
+                                        <span class="l-h__m-link-w">What we do</span>
+                                    </a>
+                                </li>
+                                <li class="l-h__m-li l-h__m-li-row-2 ">
+                                    <a href="/who-we-are.php" target="" title="Who we are"
+                                        class="l-h__m-link a-link--wline-span">
+                                        <span class="l-h__m-link-w">Who we are</span>
+                                    </a>
+                                </li>
+                                <li class="l-h__m-li l-h__m-li-row-2 ">
+                                    <a href="/insights.php" target="" title="Insights"
+                                        class="l-h__m-link a-link--wline-span">
+                                        <span class="l-h__m-link-w">Insights</span>
+                                    </a>
+                                </li>
+                                <li class="l-h__m-li l-h__m-li-row-2 ">
+                                    <a href="/careers.php" target="" title="Careers"
+                                        class="l-h__m-link a-link--wline-span">
+                                        <span class="l-h__m-link-w">Careers</span>
+                                    </a>
+                                </li>
+                                <li class="l-h__m-li">
+                                    <button class="js-modal__bt-open-form js-header__bt l-h__m-link l-h__m-link@sm"
+                                        data-gaeventlabel="Let&#39;s talk nav">
+                                        <span class="l-h__m-link-w cta_button">
+                                            Let's talk
+                                        </span>
+                                    </button>
+                                </li>
+                                <div>
+
+                                    <div class="language-switcher-language-url" id="block-languageswitcher"
+                                        role="navigation">
+
+                                        <div id="language-picker">
+                                            <div id="language-picker__current" class="l-h__m-lang-switcher-current">
+                                                <p class="en">English
+                                                    <span class="language-picker__icon">
+                                                        <svg role="presentation" aria-hidden="true"
+                                                            class=" a-icon icon-caret ">
+                                                            <use xlink:href="#icon-caret"></use>
+                                                        </svg>
+                                                    </span>
+                                                </p>
+                                            </div>
+                                            <ul id="language-picker__options" class="l-h__m-lang-options" tabindex="-1">
+                                                <li hreflang="fr-ca" class="fr-ca">
+                                                    <a href="https://www.slalom.com/fr-ca/?lp=1" class="language-link"
+                                                        hreflang="fr-ca">Français</a>
+                                                </li>
+                                                <li hreflang="ja" class="ja">
+                                                    <a href="https://www.slalom.com/ja/?lp=1" class="language-link"
+                                                        hreflang="ja">日本語</a>
+                                                </li>
+                                                <li hreflang="de-de" class="de-de">
+                                                    <a href="https://www.slalom.com/de-de/?lp=1" class="language-link"
+                                                        hreflang="de-de">Deutsch</a>
+                                                </li>
+                                            </ul>
+                                        </div>
+
+                                    </div>
+
+                                </div>
+
+                                </button>
+                                </li>
