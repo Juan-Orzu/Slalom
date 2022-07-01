f0478a0e53ecb30ce6bd103cd234f744a0c139c7
Juan Carlos Angeles Orzuna <juankyorzuna08@gmail.com>
2022-07-01 13:32:25 -0500

    Footer & menu

--- footer.php
+++ footer.php
@@ -1,101 +1,207 @@
-<footer>
-    <div class="col-6 col-sm-3">
-        <a href="/insights.php" title="Insights" class="l-f__menu-item l-f__menu-item--right">
-            Insights
-        </a>
-        <a href="/careers.php" title="Careers" class="l-f__menu-item l-f__menu-item--right">
-            Careers
-        </a>
-        <a href="/locations.php" title="Locations" class="l-f__menu-item l-f__menu-item--right">
-            Locations
-        </a>
-    </div>
-    </div>
-    </div>
-    <div class="col-12 offset-lg-4 col-lg-6 order-lg-3">
-        <span class="l-f__line"></span>
-    </div>
-    <div class="col-12 col-lg-4 order-lg-4">
-        <div class="l-f__menu l-f__menu--social">
-            <a href="https://www.facebook.com/slalomconsulting" title="Facebook" target="_blank"
-                class="l-f__menu-item l-f__menu-item--social">
-                <span class="l-f__menu-item-title">
-                    <svg role="presentation" aria-hidden="true" class=" a-icon icon-facebook ">
-                        <use xlink:href="#icon-facebook"></use>
+<?php 
+    include_once("source.php");
+?>
+
+
+<footer id="js-footer" class="l-f">
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <a href="https://www.slalom.com/" title="Slalom" class="l-f__logo">
+                    <svg role="presentation" aria-hidden="true" class=" a-icon icon-slalom ">
+                        <use xlink:href="#icon-slalom"></use>
                     </svg>
-                </span>
-            </a>
-            <a href="https://twitter.com/slalom" title="Twitter" target="_blank"
-                class="l-f__menu-item l-f__menu-item--social">
-                <span class="l-f__menu-item-title">
-                    <svg role="presentation" aria-hidden="true" class=" a-icon icon-twitter ">
-                        <use xlink:href="#icon-twitter"></use>
-                    </svg>
-                </span>
-            </a>
-            <a href="https://www.instagram.com/slalomconsulting/" title="Instagram" target="_blank"
-                class="l-f__menu-item l-f__menu-item--social">
-                <span class="l-f__menu-item-title">
-                    <svg role="presentation" aria-hidden="true" class=" a-icon icon-instagram ">
-                        <use xlink:href="#icon-instagram"></use>
-                    </svg>
-                </span>
-            </a>
-            <a href="https://www.linkedin.com/company/slalom-consulting" title="LinkedIn" target="_blank"
-                class="l-f__menu-item l-f__menu-item--social">
-                <span class="l-f__menu-item-title">
-                    <svg role="presentation" aria-hidden="true" class=" a-icon icon-linkedin ">
-                        <use xlink:href="#icon-linkedin"></use>
-                    </svg>
-                </span>
-            </a>
+                </a>
+            </div>
         </div>
     </div>
-    <div class="col-12 col-lg-3 order-lg-1">
-        <div class="l-f__content l-f__content--left">
-            <p></p>
-            <p>Have you seen what weve been working on at <a href="https://www.slalombuild.com/" rel="noopener"
-                    target="_blank">Slalom Build</a>?</p>
-            <p>&nbsp;</p>
-            <p>Visit our Medium publication <a href="https://medium.com/the-slowdown" rel="noreferrer noopener"
-                    target="_blank" title="https://medium.com/the-slowdown">The Slowdown</a> for explorations of
-                diversity, inclusion, and the future.</p>
-            <p></p>
+    <div class="container">
+        <div class="row">
+            <div class="col-12 col-lg-8 order-lg-2 order-lg-2 offset-lg-1">
+                <div class="row">
+                    <div class="col-6 col-sm-3">
+                        <span class="l-f__menu-name">
+                            <a href="https://www.slalom.com/what-we-do" target="" class="">
+                                What we do
+                            </a>
+                        </span>
+                        <div class="l-f__menu">
+                            <a href="https://www.slalom.com/what-we-do/strategy" title="Strategy"
+                                class="l-f__menu-item">
+                                Strategy
+                            </a>
+                            <a href="https://www.slalom.com/what-we-do/technology" title="Technology"
+                                class="l-f__menu-item">
+                                Technology
+                            </a>
+                            <a href="https://www.slalom.com/what-we-do/transformation" title="Transformation"
+                                class="l-f__menu-item">
+                                Transformation
+                            </a>
+                            <a href="https://www.slalom.com/industries" title="Industries" class="l-f__menu-item">
+                                Industries
+                            </a>
+                            <a href="https://www.slalom.com/case-studies" title="Case studies" class="l-f__menu-item">
+                                Case studies
+                            </a>
+                        </div>
+                    </div>
+                    <div class="col-6 col-sm-3">
+                        <span class="l-f__menu-name">
+                            <a href="https://www.slalom.com/platforms" target="" class="">
+                                Platforms
+                            </a>
+                        </span>
+                        <div class="l-f__menu">
+                            <a href="https://www.slalom.com/platforms/aws-cloud-consulting" title="AWS"
+                                class="l-f__menu-item">
+                                AWS
+                            </a>
+                            <a href="https://www.slalom.com/platforms/google-cloud" title="Google Cloud"
+                                class="l-f__menu-item">
+                                Google Cloud
+                            </a>
+                            <a href="https://www.slalom.com/platforms/microsoft" title="Microsoft"
+                                class="l-f__menu-item">
+                                Microsoft
+                            </a>
+                            <a href="https://www.slalom.com/platforms/salesforce-consulting" title="Salesforce"
+                                class="l-f__menu-item">
+                                Salesforce
+                            </a>
+                            <a href="https://www.slalom.com/platforms/snowflake" title="Snowflake"
+                                class="l-f__menu-item">
+                                Snowflake
+                            </a>
+                            <a href="https://www.slalom.com/platforms/tableau" title="Tableau" class="l-f__menu-item">
+                                Tableau
+                            </a>
+                        </div>
+                    </div>
+                    <div class="col-6 col-sm-3">
+                        <span class="l-f__menu-name">
+                            <a href="https://www.slalom.com/who-we-are" target="" class="">
+                                Who we are
+                            </a>
+                        </span>
+                        <div class="l-f__menu">
+                            <a href="https://www.slalom.com/culture" title="Culture" class="l-f__menu-item">
+                                Culture
+                            </a>
+                            <a href="https://www.slalom.com/who-we-are/diversity-and-inclusion"
+                                title="Inclusion, diversity, and equity" class="l-f__menu-item">
+                                Inclusion, diversity, and equity
+                            </a>
+                            <a href="https://www.slalom.com/who-we-are/sustainability-impact"
+                                title="Sustainability and impact" class="l-f__menu-item">
+                                Sustainability and impact
+                            </a>
+                            <a href="https://www.slalom.com/newsroom" title="Newsroom" class="l-f__menu-item">
+                                Newsroom
+                            </a>
+                        </div>
+                    </div>
+                    <div class="col-6 col-sm-3">
+                        <a href="https://www.slalom.com/insights" title="Insights"
+                            class="l-f__menu-item l-f__menu-item--right">
+                            Insights
+                        </a>
+                        <a href="https://www.slalom.com/careers" title="Careers"
+                            class="l-f__menu-item l-f__menu-item--right">
+                            Careers
+                        </a>
+                        <a href="https://www.slalom.com/locations" title="Locations"
+                            class="l-f__menu-item l-f__menu-item--right">
+                            Locations
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-12 offset-lg-4 col-lg-6 order-lg-3">
+                <span class="l-f__line"></span>
+            </div>
+            <div class="col-12 col-lg-4 order-lg-4">
+                <div class="l-f__menu l-f__menu--social">
+                    <a href="https://www.facebook.com/slalomconsulting" title="Facebook" target="_blank"
+                        class="l-f__menu-item l-f__menu-item--social">
+                        <span class="l-f__menu-item-title">
+                            <svg role="presentation" aria-hidden="true" class=" a-icon icon-facebook ">
+                                <use xlink:href="#icon-facebook"></use>
+                            </svg>
+                        </span>
+                    </a>
+                    <a href="https://twitter.com/slalom" title="Twitter" target="_blank"
+                        class="l-f__menu-item l-f__menu-item--social">
+                        <span class="l-f__menu-item-title">
+                            <svg role="presentation" aria-hidden="true" class=" a-icon icon-twitter ">
+                                <use xlink:href="#icon-twitter"></use>
+                            </svg>
+                        </span>
+                    </a>
+                    <a href="https://www.instagram.com/slalomconsulting/" title="Instagram" target="_blank"
+                        class="l-f__menu-item l-f__menu-item--social">
+                        <span class="l-f__menu-item-title">
+                            <svg role="presentation" aria-hidden="true" class=" a-icon icon-instagram ">
+                                <use xlink:href="#icon-instagram"></use>
+                            </svg>
+                        </span>
+                    </a>
+                    <a href="https://www.linkedin.com/company/slalom-consulting" title="LinkedIn" target="_blank"
+                        class="l-f__menu-item l-f__menu-item--social">
+                        <span class="l-f__menu-item-title">
+                            <svg role="presentation" aria-hidden="true" class=" a-icon icon-linkedin ">
+                                <use xlink:href="#icon-linkedin"></use>
+                            </svg>
+                        </span>
+                    </a>
+                </div>
+            </div>
+            <div class="col-12 col-lg-3 order-lg-1">
+                <div class="l-f__content l-f__content--left">
+                    <p></p>
+                    <p>Have you seen what we’ve been working on at <a href="https://www.slalombuild.com/" rel="noopener"
+                            target="_blank">Slalom Build</a>?</p>
+                    <p>&nbsp;</p>
+                    <p>Visit our Medium publication <a href="https://medium.com/the-slowdown" rel="noreferrer noopener"
+                            target="_blank" title="https://medium.com/the-slowdown">The Slowdown</a> for explorations of
+                        diversity, inclusion, and the future.</p>
+                    <p></p>
+                </div>
+            </div>
+            <div class="col-12 col-lg-2 order-lg-5">
+                <div class="l-f__menu">
+                    <a href="https://www.slalom.com/privacy-policy" title="Privacy policy" class="l-f__menu-item ">
+                        Privacy policy
+                    </a>
+                    <a href="https://www.slalom.com/terms-of-use" title="Terms of use" class="l-f__menu-item ">
+                        Terms of use
+                    </a>
+                    <a href="https://www.slalom.com/#" title="Cookie settings"
+                        class="l-f__menu-item  optanon-show-settings">
+                        Cookie settings
+                    </a>
+                </div>
+            </div>
+            <div class="col-12 col-lg-2 order-lg-7">
+                <p class="l-f__quote h-text-medium">
+                    Love your future
+                    <span class="l-footer_shape">
+                        <svg class=" a-icon icon-shape ">
+                            <use xlink:href="#icon-shape"></use>
+                        </svg>
+                    </span>
+                </p>
+            </div>
+            <div class="col-12 col-lg-4 order-lg-6">
+                <p class="l-f__content l-f__content--disclaimer">
+                </p>
+                <p><span style="font-family:arial;">©</span> 2022 Slalom, LLC</p>
+                <p><br>
+                    <br>
+                    All rights reserved
+                </p>
+                <p></p>
+            </div>
         </div>
-    </div>
-    <div class="col-12 col-lg-2 order-lg-5">
-        <div class="l-f__menu">
-            <a href="/privacy-policy.php" title="Privacy policy" class="l-f__menu-item ">
-                Privacy policy
-            </a>
-            <a href="/terms-of-use.php" title="Terms of use" class="l-f__menu-item ">
-                Terms of use
-            </a>
-            <a href="#" title="Cookie settings" class="l-f__menu-item  optanon-show-settings">
-                Cookie settings
-            </a>
-        </div>
-    </div>
-    <div class="col-12 col-lg-2 order-lg-7">
-        <p class="l-f__quote h-text-medium">
-            Love your future
-            <span class="l-footer_shape">
-                <svg class=" a-icon icon-shape ">
-                    <use xlink:href="#icon-shape"></use>
-                </svg>
-            </span>
-        </p>
-    </div>
-    <div class="col-12 col-lg-4 order-lg-6">
-        <p class="l-f__content l-f__content--disclaimer">
-        </p>
-        <p><span style="font-family:arial;">©</span> 2022 Slalom, LLC</p>
-        <p><br>
-            <br>
-            All rights reserved
-        </p>
-        <p></p>
-    </div>
-    </div>
     </div>
 </footer>
