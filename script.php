08075fc8d60e34bb440de4d0a1ae7c54055e0973
Juan Carlos Angeles Orzuna <juankyorzuna08@gmail.com>
2022-07-03 18:15:38 -0500

    Finished project

---
+++ script.php
@@ -0,0 +1,102 @@
+<script type="application/json"
+        data-drupal-selector="drupal-settings-json">{"path":{"baseUrl":"\/","scriptPath":null,"pathPrefix":"","currentPath":"node\/1","currentPathIsAdmin":false,"isFront":false,"currentLanguage":"en"},"pluralDelimiter":"\u0003","suppressDeprecationErrors":true,"dataLayer":{"defaultLang":"en","languages":{"en":{"id":"en","name":"English","direction":"ltr","weight":0},"fr-ca":{"id":"fr-ca","name":"French","direction":"ltr","weight":1},"ja":{"id":"ja","name":"Japanese","direction":"ltr","weight":2},"de-de":{"id":"de-de","name":"German","direction":"ltr","weight":3}}},"blazy":{"loadInvisible":false,"offset":100,"saveViewportOffsetDelay":50,"validateDelay":25,"loader":true,"unblazy":false,"compat":true},"blazyIo":{"disconnect":false,"rootMargin":"0px","threshold":[0,0.25,0.5,0.75,1]},"user":{"uid":0,"permissionsHash":"f802d2f69eff9611ae3486a92667afbf597ab0c91fd68722db061c3f6e3f84e9"}}</script>
+    <script
+        src="./source/js/function.js"
+        type="text/javascript"></script>
+    <script
+        src="./sources/js/jquery.js"
+        type="text/javascript"></script>
+    <script
+        src="./sources/js/classList.js"
+        type="text/javascript"></script>
+    <script
+        src="./sources/js/type.js"
+        type="text/javascript"></script>
+    <script
+        src="./sources/js/request.js"
+        type="text/javascript"></script>
+    <script
+        src="./sources/js/selector.js"
+        type="text/javascript"></script>
+    <script
+        src="./sources/js/window.js"
+        type="text/javascript"></script>
+    <script
+        src="./sources/js/element.js"
+        type="text/javascript"></script>
+    <script
+        src="./sources/js/drupal.js"
+        type="text/javascript"></script>
+    <script
+        src="./sources/js/object.js"
+        type="text/javascript"></script>
+    <script
+        src="./sources/js/chain.js"
+        type="text/javascript"></script>
+    <script
+        src="./sources/js/window_size.js"
+        type="text/javascript"></script>
+    <script
+        src="./sources/js/define.js"
+        type="text/javascript"></script>
+    <script
+        src="./sources/js/define_el.js"
+        type="text/javascript"></script>
+    <script
+        src="./sources/js/observer.js"
+        type="text/javascript"></script>
+    <script
+        src="./sources/js/loanding.js"
+        type="text/javascript"></script>
+    <script
+        src="./sources/js/script_1.js"
+        type="text/javascript"></script>
+    <script
+        src="./sources/js/script_2.js"
+        type="text/javascript"></script>
+    <script
+        src="./sources/js/script_3.js"
+        type="text/javascript"></script>
+    <script
+        src="./sources/js/script_4.js"
+        type="text/javascript"></script>
+    <script
+        src="./sources/js/script_5.js"
+        type="text/javascript"></script>
+    <script
+        src="./sources/js/script_6.js"
+        type="text/javascript"></script>
+    <script
+        src="./sources/js/script_7.js"
+        type="text/javascript"></script>
+    <script
+        src="./sources/js/script_8.js"
+        type="text/javascript"></script>
+    <script
+        src="./sources/js/script_9.js"
+        type="text/javascript">
+    </script>
+    <script src="./sources/js/jquery.dollar.js" type="text/javascript"></script>
+    <script
+        src="./sources/js/script_10.js"
+        type="text/javascript"></script>
+    <script
+        src="./sources/js/script_11.js"
+        type="text/javascript"></script>
+    <script
+        src="./sources/js/script_12.js"
+        type="text/javascript"></script>
+    <script src="./sources/js/api.js" type="text/javascript"></script>
+    <script src="./sources/js/E-v1.js" type="text/javascript"></script>
+    <script type="text/javascript">
+        window._wq = window._wq || [];
+        window._wq.push(function (W) {
+            W.consent(false);
+        });
+    </script>
+    <script type="text/plain" class="optanon-category-C0003">
+      window._wq = window._wq || [];
+      window._wq.push(function (W) {
+        W.consent(true);
+      });
+    </script>
